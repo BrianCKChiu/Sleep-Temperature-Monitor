1111dedc89c3699580de331315b150581e67d375
Brian Chiu <brianchiu33@gmail.com>
2023-11-30 23:05:31 -0500

    udpated host IP addr

--- server/app.py
+++ server/app.py
@@ -3,13 +3,15 @@ import threading
 from database import initialize_db
 from flask import Flask, request, jsonify
 from process import handle_request
+
 app = Flask(__name__)
-PORT=5000
+PORT = 5000
 
 
 @app.route("/health", methods=["GET"])
 def health():
     return jsonify({"status": "ok"}), 200
+
 
 @app.route("/api/v1/update", methods=["POST"])
 def update():
@@ -21,15 +23,15 @@ def update():
     return jsonify({"status": "ok"}), 200
 
 
-
 def validate_update_json(data):
     if "client_id" not in data:
         return False
     else:
         return True
-    
+
+
 if __name__ == "__main__":
     initialize_db()
-   
-    app.run(host="127.0.0.1", port=PORT, debug=True)
+
+    app.run(host="0.0.0.0", port=PORT, debug=True)
     print("Server started on port {}".format(PORT))
